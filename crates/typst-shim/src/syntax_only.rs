ddf5c272bf169d39cad62172349b8a7f168df78c
Maximus Gorog <mgorog@gmail.com>
2026-03-14 18:31:00 -0600

    adapt-to-forked-typst

--- crates/typst-shim/src/syntax_only.rs
+++ crates/typst-shim/src/syntax_only.rs
@@ -1,34 +1,93 @@
-use std::sync::atomic::{AtomicBool, Ordering};
-use typst::{
-    Document, World,
-    diag::{SourceDiagnostic, SourceResult, Warned},
-    ecow::eco_vec,
-};
-use typst_syntax::Span;
+//! Typst Evaluation
 
-/// Global flag indicating whether syntax-only mode is enabled.
-pub static SYNTAX_ONLY: AtomicBool = AtomicBool::new(false);
+use comemo::Track;
+use typst::World;
+use typst::diag::SourceResult;
+use typst::engine::{Engine, Route, Sink, Traced};
+use typst::foundations::{Context, Func, Module, Value};
+use typst::introspection::Introspector;
+use typst::syntax::Source;
+pub use typst_eval::*;
 
-/// Check if syntax-only mode is enabled.
-pub fn is_syntax_only() -> bool {
-    SYNTAX_ONLY.load(Ordering::Acquire)
+//ID10T start
+use crate::utils::Protected;
+//ID10T end
+
+//ID10T start
+use typst_library::introspection::EmptyIntrospector;
+//ID10T end
+
+/// Evaluates a source file and return the resulting module.
+pub fn eval_compat(world: &dyn World, source: &Source) -> SourceResult<Module> {
+    let route = Route::default();
+    let traced = Traced::default();
+    let mut sink = Sink::default();
+    typst_eval::eval(
+        &typst::ROUTINES,
+        world.track(),
+        traced.track(),
+        sink.track_mut(),
+        route.track(),
+        source,
+    )
 }
 
-/// Compile the document if syntax-only mode is disabled; otherwise, return an
-/// error.
-pub fn compile_opt<D>(world: &dyn World) -> Warned<SourceResult<D>>
-where
-    D: Document,
-{
-    if is_syntax_only() {
-        Warned {
-            output: Err(eco_vec![SourceDiagnostic::error(
-                Span::detached(),
-                "Compilation is disabled in syntax-only mode.",
-            )]),
-            warnings: Default::default(),
+/// The Typst Engine.
+pub struct TypstEngine<'a> {
+    /// The introspector to be queried for elements and their positions.
+    //ID10T start
+    pub introspector: Box<dyn Introspector>,
+    //ID10T end
+    /// May hold a span that is currently under inspection.
+    pub traced: Traced,
+    /// The route the engine took during compilation. This is used to detect
+    /// cyclic imports and excessive nesting.
+    pub route: Route<'static>,
+    /// A push-only sink for delayed errors, warnings, and traced values.
+    ///
+    /// All tracked methods of this type are of the form `(&mut self, ..) ->
+    /// ()`, so in principle they do not need validation (though that
+    /// optimization is not yet implemented in comemo).
+    pub sink: Sink,
+    /// The environment in which typesetting occurs.
+    pub world: &'a dyn World,
+}
+
+impl<'a> TypstEngine<'a> {
+    /// Creates a new Typst Engine.
+    pub fn new(world: &'a dyn World) -> Self {
+        Self {
+            //ID10T start
+            introspector: Box::new(EmptyIntrospector {}),
+            //ID10T end
+            traced: Traced::default(),
+            route: Route::default(),
+            sink: Sink::default(),
+            world,
         }
-    } else {
-        typst::compile::<D>(world)
+    }
+
+    /// Creates the engine.
+    pub fn as_engine(&'a mut self) -> Engine<'a> {
+        Engine {
+            routines: &typst::ROUTINES,
+            world: self.world.track(),
+            //ID10T start
+            introspector: Protected::new(self.introspector.as_ref().track()),
+            //ID10T end
+            traced: self.traced.track(),
+            sink: self.sink.track_mut(),
+            route: self.route.clone(),
+        }
+    }
+
+    /// Applies a function.
+    pub fn apply(&'a mut self, func: &Func, ctx: Context, args: Vec<Value>) -> SourceResult<Value> {
+        func.call(&mut self.as_engine(), ctx.track(), args)
+    }
+
+    /// Calls a function.
+    pub fn call(&'a mut self, func: &Func, ctx: Context) -> SourceResult<Value> {
+        self.apply(func, ctx, vec![])
     }
 }
